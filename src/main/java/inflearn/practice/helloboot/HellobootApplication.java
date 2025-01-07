d135c8f1f4e6ed57d9226a8866fafc0a37622ac7
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-07 23:52:13 +0900

    서블릿 등록

--- src/main/java/inflearn/practice/helloboot/HellobootApplication.java
+++ src/main/java/inflearn/practice/helloboot/HellobootApplication.java
@@ -1,14 +1,27 @@
 package inflearn.practice.helloboot;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import java.io.IOException;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();// 톰캣 웹서버 생성
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("hello", new HttpServlet() {
+            @Override
+            protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.setStatus(200);
+                resp.setHeader("Content-Type", "text/plain");
+                resp.getWriter().println("Hello Servlet");
+            }
+        }).addMapping("/hello"));
         webServer.start();
     }
 
