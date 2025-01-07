54f8b775c70ba251441ecdd2619d804e816b65c3
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-07 23:40:29 +0900

    Containerless 개발 준비
    - Main 메서드부터 시작

--- src/main/java/inflearn/practice/helloboot/HellobootApplication.java
+++ src/main/java/inflearn/practice/helloboot/HellobootApplication.java
@@ -3,11 +3,9 @@ package inflearn.practice.helloboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
     }
 
 }
