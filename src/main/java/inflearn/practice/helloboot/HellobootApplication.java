1393365b3a1ba6a9145ba79ee88de69ecf986e26
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-07 23:45:53 +0900

    톰캣 서블릿 컨테이너 띄우기

--- src/main/java/inflearn/practice/helloboot/HellobootApplication.java
+++ src/main/java/inflearn/practice/helloboot/HellobootApplication.java
@@ -1,9 +1,15 @@
 package inflearn.practice.helloboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Containerlee Standalone Application");
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();// 톰캣 웹서버 생성
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
