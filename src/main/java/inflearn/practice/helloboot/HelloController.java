a11727bc51316b95def4bbdcd64a4abd8d68d211
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-09 23:16:13 +0900

    HelloController로 위임

--- src/main/java/inflearn/practice/helloboot/HelloController.java
+++ src/main/java/inflearn/practice/helloboot/HelloController.java
@@ -1,13 +1,7 @@
 package inflearn.practice.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam("name") String name) {
+    public String hello(String name) {
         return "Hello " + name;
     }
 }
