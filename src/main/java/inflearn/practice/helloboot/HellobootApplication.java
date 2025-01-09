334156f5886645fc30959d89cc6fda615aa782ce
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-10 00:53:27 +0900

    Bean Factory 메서드 방식으로 빈 등록

--- src/main/java/inflearn/practice/helloboot/HellobootApplication.java
+++ src/main/java/inflearn/practice/helloboot/HellobootApplication.java
@@ -3,12 +3,25 @@ package inflearn.practice.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class HellobootApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -22,8 +35,7 @@ public class HellobootApplication {
             }
         };
 
-        applicationContext.registerBean(HelloController.class); // 빈 등록
-        applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
+        applicationContext.register(HellobootApplication.class);
         applicationContext.refresh(); // 초기화해서 빈 객체 생성
     }
 
