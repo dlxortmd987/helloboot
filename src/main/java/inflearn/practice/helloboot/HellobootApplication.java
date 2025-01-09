164f0be434bfc727a9949792aad2c0c248547f82
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-10 00:48:49 +0900

    applicationContext 초기화 시점에 servletContext 생성

--- src/main/java/inflearn/practice/helloboot/HellobootApplication.java
+++ src/main/java/inflearn/practice/helloboot/HellobootApplication.java
@@ -8,17 +8,23 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();// 톰캣 웹서버 생성
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
+                            .addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+
         applicationContext.registerBean(HelloController.class); // 빈 등록
         applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
         applicationContext.refresh(); // 초기화해서 빈 객체 생성
-
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();// 톰캣 웹서버 생성
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
-                    .addMapping("/*");
-        });
-        webServer.start();
     }
 
 }
