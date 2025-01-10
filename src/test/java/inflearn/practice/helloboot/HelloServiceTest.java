836d218ee833628796aac361271b012d11565b3b
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-10 23:27:53 +0900

    decorator를 통한 추가 기능

--- src/test/java/inflearn/practice/helloboot/HelloServiceTest.java
+++ src/test/java/inflearn/practice/helloboot/HelloServiceTest.java
@@ -12,4 +12,13 @@ class HelloServiceTest {
 
         Assertions.assertThat(ret).isEqualTo("Hello Test");
     }
+
+    @Test
+    void helloDecorator() {
+        HelloDecorator decorator = new HelloDecorator(name -> name);
+
+        String ret = decorator.sayHello("Test");
+
+        Assertions.assertThat(ret).isEqualTo("*Test*");
+    }
 }
