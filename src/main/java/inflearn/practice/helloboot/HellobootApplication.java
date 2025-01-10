9ec736d845d3347cc339ca52ec91699b812ea856
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-10 22:56:53 +0900

    SpringBootApplication 형식으로 리팩토링
    기존 것과 비교

--- src/main/java/inflearn/practice/helloboot/HellobootApplication.java
+++ src/main/java/inflearn/practice/helloboot/HellobootApplication.java
@@ -1,12 +1,10 @@
 package inflearn.practice.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -23,25 +21,6 @@ public class HellobootApplication {
     }
 
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//                dispatcherServlet.setApplicationContext(this); ApplicationContextAware를 구현하면 Spring Container가 자동으로 주입해줌
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-
-        applicationContext.register(HellobootApplication.class);
-        applicationContext.refresh(); // 초기화해서 빈 객체 생성
+        MySpringApplication.run(HellobootApplication.class, args);
     }
-
 }
