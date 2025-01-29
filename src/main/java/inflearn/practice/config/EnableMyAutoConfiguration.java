95bada166cdc8f43b8caf826c59ac0d6ab16ad4c
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-29 22:49:58 +0900

    동적으로 설정 파일 등록

--- src/main/java/inflearn/practice/config/EnableMyAutoConfiguration.java
+++ src/main/java/inflearn/practice/config/EnableMyAutoConfiguration.java
@@ -1,7 +1,5 @@
 package inflearn.practice.config;
 
-import inflearn.practice.config.autoconfig.DispatcherServletConfig;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
@@ -11,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({DispatcherServletConfig.class, TomcatServletWebServerFactory.class})
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 }
