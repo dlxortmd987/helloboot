97c5000dea7daed18b7859df53e10817826c4c62
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-30 19:08:04 +0900

    @Configuration의 proxyBeanMethods에 대한 이해

--- src/main/java/inflearn/practice/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/inflearn/practice/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package inflearn.practice.config.autoconfig;
 
+import inflearn.practice.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
---
+++ src/test/java/inflearn/practice/study/ConfigurationTest.java
@@ -0,0 +1,89 @@
+package inflearn.practice.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+public class ConfigurationTest {
+    @Test
+    void 직접_객체를_생성하면_common이_다르게_생성된다() {
+        MyConfig myConfig = new MyConfig();
+        Bean1 bean1 = myConfig.bean1();
+        Bean2 bean2 = myConfig.bean2();
+
+        Assertions.assertThat(bean1.common).isNotSameAs(bean2.common);
+    }
+
+    @Test
+    void Spring_Context에_등록하면_common이_동일하게_생성된다() {
+        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+        ac.register(MyConfig.class);
+        ac.refresh();
+
+        Bean1 bean1 = ac.getBean(Bean1.class);
+        Bean2 bean2 = ac.getBean(Bean2.class);
+
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    @Test
+    void proxy객체를_이용하면_Spring과_동일한_결과가_나온다() {
+        MyConfigProxy myConfigProxy = new MyConfigProxy();
+
+        Bean1 bean1 = myConfigProxy.bean1();
+        Bean2 bean2 = myConfigProxy.bean2();
+
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    static class MyConfigProxy extends MyConfig {
+        private Common common;
+
+        @Override
+        Common common() {
+            if (this.common == null) {
+                this.common = super.common();
+            }
+
+            return this.common;
+        }
+    }
+
+    @Configuration
+    static class MyConfig {
+        @Bean
+        Common common() {
+            return new Common();
+        }
+
+        @Bean
+        Bean1 bean1() {
+            return new Bean1(common());
+        }
+
+        @Bean
+        Bean2 bean2() {
+            return new Bean2(common());
+        }
+    }
+
+    static class Bean1 {
+        private final Common common;
+
+        Bean1(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Bean2 {
+        private final Common common;
+
+        Bean2(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Common {}
+}
