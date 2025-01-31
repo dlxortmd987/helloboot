58e94ad5a5632d4f140c463afb8dca0c0d476005
Taek Seung Lee <dlxortmd987@gmail.com>
2025-01-31 23:15:09 +0900

    @Conditional 학습 테스트 - 2(어노테이션 조건 설정)

--- src/test/java/inflearn/practice/study/ConditionalTest.java
+++ src/test/java/inflearn/practice/study/ConditionalTest.java
@@ -10,6 +10,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Map;
 
 public class ConditionalTest {
 
@@ -33,18 +34,13 @@ public class ConditionalTest {
 
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.TYPE)
-    @Conditional(TrueCondition.class)
-    @interface TrueConditional {
-    }
-
-    @Retention(RetentionPolicy.RUNTIME)
-    @Target(ElementType.TYPE)
-    @Conditional(FalseCondition.class)
-    @interface FalseConditional {
+    @Conditional(BooleanCondition.class)
+    @interface BooleanConditional {
+        boolean value();
     }
 
     @Configuration
-    @TrueConditional
+    @BooleanConditional(true)
     static class Config1 {
         @Bean
         MyBean myBean() {
@@ -53,7 +49,7 @@ public class ConditionalTest {
     }
 
     @Configuration
-    @FalseConditional
+    @BooleanConditional(false)
     static class Config2 {
         @Bean
         MyBean myBean() {
@@ -66,17 +62,12 @@ public class ConditionalTest {
 
     }
 
-    static class TrueCondition implements Condition {
+    static class BooleanCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
-        }
-    }
-
-    static class FalseCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
+            Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(BooleanConditional.class.getName());
+            Boolean value = (Boolean) annotationAttributes.get("value");
+            return value;
         }
     }
 }
