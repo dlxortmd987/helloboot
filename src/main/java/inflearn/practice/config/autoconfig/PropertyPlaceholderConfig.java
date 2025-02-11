41e967dd3cb7c5802c57754300f40bf0db40cebb
Taek Seung Lee <dlxortmd987@gmail.com>
2025-02-11 22:21:22 +0900

    @Value 사용을 위한 PropertyPlaceholderConfig 설정

---
+++ src/main/java/inflearn/practice/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,13 @@
+package inflearn.practice.config.autoconfig;
+
+import inflearn.practice.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
