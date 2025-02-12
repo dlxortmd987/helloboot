ba32ecc20e460fac364b7322dbf4516297774df7
Taek Seung Lee <dlxortmd987@gmail.com>
2025-02-12 23:57:24 +0900

    DataSource 설정

---
+++ src/main/java/inflearn/practice/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package inflearn.practice.config.autoconfig;
+
+import inflearn.practice.config.ConditionalMyOnClass;
+import inflearn.practice.config.EnableMyConfigurationProperties;
+import inflearn.practice.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)
+public class DataSourceConfig {
+    @Bean
+    DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+}
