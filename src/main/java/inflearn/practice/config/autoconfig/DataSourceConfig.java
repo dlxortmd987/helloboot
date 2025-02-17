0895b96269c9b0dadff5feef18f56c5b2e478f4d
Taek Seung Lee <dlxortmd987@gmail.com>
2025-02-17 23:03:15 +0900

    Hikari DataSource 설정

--- src/main/java/inflearn/practice/config/autoconfig/DataSourceConfig.java
+++ src/main/java/inflearn/practice/config/autoconfig/DataSourceConfig.java
@@ -1,8 +1,10 @@
 package inflearn.practice.config.autoconfig;
 
+import com.zaxxer.hikari.HikariDataSource;
 import inflearn.practice.config.ConditionalMyOnClass;
 import inflearn.practice.config.EnableMyConfigurationProperties;
 import inflearn.practice.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
@@ -14,11 +16,26 @@ import java.sql.Driver;
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
 public class DataSourceConfig {
     @Bean
+    @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
+    @ConditionalOnMissingBean
     DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 
         dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
         dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    DataSource hikariDataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setDriverClassName(properties.getDriverClassName());
+        dataSource.setJdbcUrl(properties.getUrl());
         dataSource.setUsername(properties.getUsername());
         dataSource.setPassword(properties.getPassword());
 
